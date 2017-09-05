a771e67f6a4375a417db1ce7febd4681b6eec587
Шумилин Александр Владимирович <a.v.shumilin@voentelecom.ru>
2017-09-05 16:12:08 +0300

    Proxy annotation

--- src/main/java/quoters/Main.java
+++ src/main/java/quoters/Main.java
@@ -4,9 +4,13 @@ package quoters;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
-        context.getBean(TerminatorQuoter.class).sayQuote();
+        while (true){
+            Thread.sleep(1000);
+//            context.getBean(TerminatorQuoter.class).sayQuote(); //по интерфейсу, а не по классу.
+            context.getBean(Quoter.class).sayQuote();
+        }
 
     }
 }
--- src/main/java/quoters/TerminatorQuoter.java
+++ src/main/java/quoters/TerminatorQuoter.java
@@ -3,6 +3,7 @@ package quoters;
 
 import javax.annotation.PostConstruct;
 
+@Profiling
 public class TerminatorQuoter implements Quoter {
     @InjectRandomInt(min=2, max=7)
     private int repeat;
